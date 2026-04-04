c83f036a69cade6e23fb336b7baa6849d977b808
goosenjuice <zamirkononov@gmail.com>
2026-04-04 17:28:52 +0300

    api.ts

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,4 +1,4 @@
-import {createApi, fetchBaseQuery} from "@reduxjs/toolkit/query/react";
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export type TaskStatus = "todo" | "in_progress" | "done";
 
@@ -13,8 +13,23 @@ export type Task = {
 
 export const api = createApi({
     reducerPath: "api",
-    baseQuery: fetchBaseQuery({ baseUrl: "/api" }),
-    endpoints: (build) => {
-        getTasks: build.query<Task[]>
-    }
-})
+    baseQuery: fetchBaseQuery({ baseUrl: "/api" }), // позже подключим MSW или реальный бэк
+    endpoints: (build) => ({
+        getTasks: build.query<Task[], { teamId: string; status?: TaskStatus; q?: string; page?: number }>({
+            query: ({ teamId, status, q, page }) => {
+                const params = new URLSearchParams();
+                if (status) params.set("status", status);
+                if (q) params.set("q", q);
+                if (page) params.set("page", String(page));
+                const qs = params.toString();
+                return `teams/${teamId}/tasks${qs ? `?${qs}` : ""}`;
+            },
+        }),
+    }),
+});
+
+export const getTask: build.query<Task, {taskId: string}> = () => {
+
+}
+
+export const { useGetTasksQuery } = api;
