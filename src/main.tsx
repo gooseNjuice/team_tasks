1f08e304b0315f0cbec8f5736efbe4f2cc7d1883
goosenjuice <zamirkononov@gmail.com>
2026-04-04 17:27:51 +0300

    initial state displaying

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,24 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import {StrictMode} from 'react'
+import {createRoot} from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import {Provider} from "react-redux";
+import {store} from "./app/store.ts";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+async function enableMocking() {
+    if (!import.meta.env.DEV) return;
+    const {worker} = await import("./mocks/browser");
+    await worker.start({
+        onUnhandledRequest: "bypass",
+    });
+}
+
+enableMocking().then(() => {
+    createRoot(document.getElementById('root')!).render(
+        <StrictMode>
+            <Provider store={store}>
+                <App/>
+            </Provider>
+        </StrictMode>,
+    )
+})
